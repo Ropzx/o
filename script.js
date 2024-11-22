4961cbc14d2b3298add92c4ada1bce5a0720a359
ropzx <127341230+Ropzx@users.noreply.github.com>
2024-11-22 16:20:42 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const correctUsername = "user";  
+const correctPassword = "pass";  
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === correctUsername && password === correctPassword) {
+        window.location.href = "o.html";  
+    } else {
+        document.getElementById("error-message").innerText = "Invalid username or password!";
+    }
+});
