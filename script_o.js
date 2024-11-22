784eb584c2d9bd838595a76dcd7295036eb3217b
ropzx <127341230+Ropzx@users.noreply.github.com>
2024-11-22 16:33:24 +0200

    Create script_o.js

---
+++ script_o.js
@@ -0,0 +1,5 @@
+// Check if the user is logged in
+if (sessionStorage.getItem("loggedIn") !== "true") {
+    // If not logged in, redirect to the login page
+    window.location.href = "index.html";
+}
